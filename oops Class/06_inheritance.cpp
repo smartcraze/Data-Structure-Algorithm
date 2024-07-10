43378bb4657b2fe6a5521711f6bb0fd59b1f089a
smartcraze <smartcraze354@gmail.com>
2024-07-10 21:50:12 +0530

    day 09 oops completed

--- oops Class/06_inheritance.cpp
+++ oops Class/06_inheritance.cpp
@@ -1,7 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
+// base class
+class Vehicle
+{
+public:
+    string brand = "Ford";
+    void honk()
+    {
+        cout << "Tuut, tuut! \n";
+    }
+};
 
-int main() {
-    
+// Derived class
+class Car : public Vehicle
+{
+public:
+    string model = "Mustang";
+};
+
+// Multi level inheritance
+//  Base class(parent) class MyClass
+class MyClass
+{
+public:
+    void myFunction()
+    {
+        cout << endl
+             << "Some content in parent class.";
+    }
+};
+
+// Derived class (child)
+class MyChild : public MyClass
+{
+};
+
+// Derived class (grandchild)
+class MyGrandChild : public MyChild
+{
+};
+
+// Another base class
+class MyOtherClass
+{
+public:
+    void myOtherFunction()
+    {
+        cout << "Some content in another class.";
+    }
+};
+
+// Derived class
+class MyChildClass : public MyClass, public MyOtherClass
+{
+};
+
+int main()
+{
+    Car myCar;
+    myCar.honk();
+    cout << myCar.brand + " " + myCar.model;
+    MyGrandChild myObj;
+    myObj.myFunction();
+    MyChildClass myObj1;
+    myObj1.myFunction();
+    myObj1.myOtherFunction();
     return 0;
 }
