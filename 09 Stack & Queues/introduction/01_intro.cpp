a3b4adc9b5fb8b2bf611ad36eb61f51661d1a1c9
smartcraze <smartcraze354@gmail.com>
2024-07-17 05:03:45 +0530

    day 12 of learning the dsa

---
+++ 09 Stack & Queues/introduction/01_intro.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    stack<int>mystack;
+    mystack.pop();
+    mystack.push(1);
+    mystack.push(2);
+    mystack.push(3);
+    mystack.top();
+    mystack.size();
+
+    // cout << mystack;
+
+    return 0;
+}
---
+++ Docs/Stack/intro.md
@@ -0,0 +1,14 @@
+### push
+
+<!-- push(2)
+push(3)
+push(4)
+push(5)
+pop()
+top()
+size() 
+explain all
+
+-->
+
+
